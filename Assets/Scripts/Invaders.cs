e095dd84aec8914883962ddafccfd6c19a9f20c6
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2024-11-14 11:23:49 +0100

    Ajout du mouvement des envahisseurs
    Ajout d'une variable `speed` de type `float` initialisée à 1.0f.
    Ajout d'une variable privée `_direction` de type `Vector3` initialisée à `Vector3.right`.
    Ajout de la méthode `Update` pour mettre à jour la position des envahisseurs en fonction de la direction et de la vitesse, et vérifier s'ils atteignent les bords de l'écran pour changer de direction et avancer d'une rangée.
    Ajout de la méthode `AdvanceRow` pour inverser la direction des envahisseurs et les faire descendre d'une rangée.
    
    Ces modifications ajoutent la capacité de mouvement aux envahisseurs dans le jeu. Les envahisseurs se déplacent horizontalement et changent de direction lorsqu'ils atteignent les bords de l'écran, tout en descendant d'une rangée à chaque changement de direction.

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -7,6 +7,10 @@ public class Invaders : MonoBehaviour
     public Invader[] prefabs;
     public int row = 5;
     public int column = 11;
+    public float speed = 1.0f;
+
+    private Vector3 _direction = Vector3.right;
+
 
     private void Awake()
     {
@@ -24,5 +28,38 @@ public class Invaders : MonoBehaviour
                 invader.transform.localPosition = position;
             }
         }
+    }
+    private void Update()
+    {
+        transform.position += _direction * speed * Time.deltaTime;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach (Transform invader in transform)
+        {
+            if (!invader.gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+            if (_direction == Vector3.right && invader.position.x >=  rightEdge.x - 1.0f)
+            {
+                AdvanceRow();
+            }
+            else if (_direction == Vector3.left && invader.position.x <= leftEdge.x + 1.0f)
+            {
+                AdvanceRow();
+                
+            }
+        }
+    }
+    private void AdvanceRow()
+    {
+        _direction.x *= -1.0f;
+
+
+        Vector3 position = transform.position;
+        position.y -= 1.0f;
+        transform.position = position;
     }
 }
